8427e67df678ff6044422048cd5958fb23f8388b
grossamos <email@amosgross.com>
2021-03-13 21:16:23 +0100

    added a welcome page

--- lib/components/main_page/main_content_pane.dart
+++ lib/components/main_page/main_content_pane.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:weight_track_app/navigation/main_route_constants.dart';
 import 'dart:ui' as ui;
-
-// TODO fix back button not updating icon in nav bar
-// TODO implement Navigators for Home, Analytics and Settings separately
 
 List<int> historyOfNavBar = [];
 
 // StatefulWidget that renders Icons, labels, etc
 class MainContentPane extends StatefulWidget {
-
   @override
   _MainContentPaneState createState() => _MainContentPaneState();
 }
@@ -21,54 +20,123 @@ class _MainContentPaneState extends State<MainContentPane> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Stack(
-      children: [
-        Positioned(
-          child: WillPopScope(
-            onWillPop: () async {
-              if (mainNavigatorKey.currentState.canPop()) {
-                historyOfNavBar.removeLast();
-                if (historyOfNavBar.last != _currentNavBarIndex)
-                  _currentNavBarIndex = historyOfNavBar.last;
-                mainNavigatorKey.currentState.pop();
-                return false;
-              }
-              return true;
-            },
-            child: Navigator(
-              key: mainNavigatorKey,
-              initialRoute: '/',
-              onGenerateRoute: mainNavigatorRoutes,
-              //TODO implement Transition here
-            ),
-          ),
-        ),
-        Positioned(
-          bottom: 0,
-          left: 0,
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 12),
-              child: Card(
-                elevation: 20,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18),
+    return FutureBuilder(
+      future: getMainScreen(),
+      builder: (BuildContext futureContext, AsyncSnapshot<Widget> widget){
+        if (widget.data == null)
+          return Container();
+        return widget.data;
+      },
+    );
+  }
+
+  Future<Widget> getMainScreen() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool showWelcomeScreen = true;
+    // welcome page 
+    if (prefs.getBool('seen') != null)
+      showWelcomeScreen = prefs.getBool('seen');
+    print(showWelcomeScreen);
+    if (showWelcomeScreen){
+      prefs.setBool('seen', false);
+      return Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  height: 300,
+                  child: SvgPicture.asset('assets/welcome.svg', semanticsLabel: 'welcome image',)
+              ),
+              SizedBox(
+                height: 41,
+              ),
+              Text('Welcome',
+                style: TextStyle(
+                  fontFamily: GoogleFonts.raleway().fontFamily,
+                  fontSize: 24
                 ),
-                child: Container(
-                  height: 72.0,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: getNavBarIcons(),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+                child: Text('Get started by adding your  split and exercises in the settings menu',
+                  style: TextStyle(
+                    fontFamily: GoogleFonts.raleway().fontFamily,
+                    fontSize: 18,
+                    color: Color(0xff8E8E8E),
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  FlatButton(onPressed: () => setState((){}), child: Text('Get Started', style: TextStyle(
+                    fontFamily: GoogleFonts.raleway().fontFamily,
+                    fontSize: 18,
+                    color: Color(0xff4A664A),
+                    decoration: TextDecoration.underline,
+                  ),
+                  textAlign: TextAlign.end,)),
+                ],
+              )
+            ],
+          ),
+        )
+      );
+    } else {
+      return Scaffold(
+          body: Stack(
+            children: [
+              Positioned(
+                child: WillPopScope(
+                  onWillPop: () async {
+                    if (mainNavigatorKey.currentState.canPop()) {
+                      historyOfNavBar.removeLast();
+                      if (historyOfNavBar.last != _currentNavBarIndex)
+                        _currentNavBarIndex = historyOfNavBar.last;
+                      mainNavigatorKey.currentState.pop();
+                      return false;
+                    }
+                    return true;
+                  },
+                  child: Navigator(
+                    key: mainNavigatorKey,
+                    initialRoute: '/',
+                    onGenerateRoute: mainNavigatorRoutes,
+                    //TODO implement Transition here
                   ),
                 ),
               ),
-            ),
-          ),
-        )
-      ],
-    ));
+              Positioned(
+                bottom: 0,
+                left: 0,
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+                    child: Card(
+                      elevation: 20,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                      child: Container(
+                        height: 72.0,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: getNavBarIcons(),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ));
+    }
   }
 
   List<Widget> getNavBarIcons() {
